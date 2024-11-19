be693781e247b2b816c489cec9aa79f58811db35
JayB111 <jay.ljungby@gmail.com>
2024-11-19 18:10:10 +0100

    first commit

---
+++ GymMembership.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class GymMembership
+{
+    public static int CalculateMembershipPrice(int age, bool wantsPremium)
+    {
+        int basePrice;
+
+        
+        if (age < 19 || age > 64)
+        {
+            basePrice = 2200;
+        }
+        else
+        {
+            basePrice = 3100;
+        }
+
+       
+        if (wantsPremium)
+        {
+            basePrice += 300;
+        }
+
+        return basePrice;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int age = 19; 
+        bool wantsPremium = true;
+
+        int price = CalculateMembershipPrice(age, wantsPremium);
+
+       
+        Console.WriteLine($"The total price is: {price}");
+    }
+}
+
